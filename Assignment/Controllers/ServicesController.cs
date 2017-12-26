bd1b0ad829dd4fafe1ab212e6326c41a7ae8e526
shaswami <shaswami@DIN76000823>
2017-12-26 10:47:52 +0530

    Add project files.

---
+++ Assignment/Controllers/ServicesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: Services
+        public ActionResult Service()
+        {
+            return View();
+        }
+    }
+}
